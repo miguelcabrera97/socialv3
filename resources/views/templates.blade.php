a7de8e642d8b5e653a28f66dbc2cf51d3939babc
miguelcabrera97 <migue122747@gmail.com>
2022-07-14 11:08:21 -0500

    mas cambios jeje

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -28,9 +28,9 @@
                     <a target="_blank" class="m-2  items-center py-2 px-2 text-sm text-center text-white bg-purple-700 rounded-lg hover:bg-purple-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"  href="/crear/{{$template->template_id}}">Crear</a>
                     </div>
 
-                </div>-->
+                </div>
                 
-                <div class="caja shadow-xl rounded-3xl max-w-lg max-h-full pt-4 pb-5 px-4">
+                <div class="caja shadow-xl rounded-3xl max-w-lg max-h-full pt-4 pb-4 px-4">
                     <div class="w-full h-100">
                         <img class="" src="{{$template->desktop_thumbnail_url}}" alt="">
                         
@@ -41,7 +41,41 @@
                     
                    
                   
-                </div>
+                </div>-->
+                {{-- CARD --}}
+         <div class=" grid grid-cols-1  mt-3 h-4/6 ">
+            <div class="flex  flex-col"> {{--  Flex --}}
+ 
+                 {{-- Card Title--}}
+                 <div class="  rounded-t-2xl bg-white h-1/6 w-full text-center  "> 
+                     <p class="text-sm capitalize my-3">{{$template->template_name}}</p>
+                 </div>
+                  {{-- End Card Title--}}
+                  
+                 <div class="  h-full w-full relative ">
+                     {{-- Botones de Creación y preview --}}
+                     <div class="  absolute rounded-b-2xl  backdrop-blur-sm bg-black/50 h-full w-full opacity-0 hover:opacity-100 hover:ease-all hover:duration-300 ">
+                         <div class="grid grid-cols-1 gap-1 justify-items-center" style="padding-top: 40%">
+                            
+                            <a target="_blank" class="m-2 w-3/5  items-center p-3 text-md text-center font-bold text-white bg-fuchsia-500 hover:bg-fuchsia-400 rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"  href="crear/{{$template->template_id}}">Empezar a Crear</a>
+                            <a class="w-3/5 m-2  items-center p-3 text-md text-center text-white rounded-lg hover:underline-offset-8 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:underline" target="_blank" href="{{$template->preview_url}}">Vista Previa</a>
+                                 
+                         
+                           
+                         </div>  
+                     </div>
+                      {{-- END Botones de Creación y preview --}}
+                     
+                     {{-- background Image Div--}}
+                     <div class="shadow-xl bg-cover bg-center rounded-b-2xl h-full w-full opacity-1 bg-white" >
+                         <img class="rounded-t-3xl rounded-b-3xl" src="{{$template->desktop_thumbnail_url}}" alt="Imagen-Fondo" border="0">
+                     </div>
+                     {{-- END background Image Div--}}
+ 
+                 </div>
+            </div>{{-- End Flex --}}
+         </div>
+          {{--  END CARD --}}
                  
                  
                 
